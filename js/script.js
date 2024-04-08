b2924cbf96e12e4f75158b02f21d973e93fb526f
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-08 15:28:20 +0200

    Vue-Email-List : Coding

--- js/script.js
+++ js/script.js
@@ -3,11 +3,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            emails : [],
+            numEmails : 10,
+            apiPath : 'https://flynn.boolean.careers/exercises/api/random/mail'
         }
     },
     methods: {
-
+        generateRndEmails(){
+            const emails = [];
+            for (let i = 0; i < this.numEmails; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+                
+                console.log(res.data);
+                
+                const email = res.data.response
+                emails.push(email);
+                })
+                
+            }
+        }
     },
     computed() {
 
